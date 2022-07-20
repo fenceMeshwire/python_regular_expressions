7334cf8a90e0ab2e189f303f30e63da1e3a12109
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-20 22:58:31 +0200

    Update 01_exact_matching.py

--- 01_exact_matching.py
+++ 01_exact_matching.py
@@ -7,18 +7,27 @@
 # Dependency
 import re
 
-sample_text = """A quiet summer morning had risen up upon the great city. The clock in
+sample_text:str = """A quiet summer morning had risen up upon the great city. The clock in
 the modern church steeple of the downtown district had not yet struck
 seven, when the side door of one of the small buildings closed and a woman
 came out into the silent street."""
 
-# Exact matching
+# EXACT MATCHING:
 result:list = re.findall("silent", sample_text)
 print(result)
 
-# Access the word in the string
+# Access the word in the given string:
 start:int = re.search("silent", sample_text).start()
 end:int = re.search("silent", sample_text).end()
 
 word:str = sample_text[start:end] 
 print(word)
+
+# COMBINE THE KEYWORD WITH ADDITIONAL CHARACTERS INTO A SEARCH PHRASE:
+sample_text:str = ""The spreadsheet contains information about the Vehicle Identification Number (VIN)."
+
+keyword:str = 'VIN'
+search_phrase:str = r'\s' + '\(' + keyword + '\)'
+
+finding:list = re.findall(search_phrase, text)
+print(finding)
