f690d0f7165afeb3614404d8c0932a99ef6be6c6
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-02-04 11:16:32 +0100

    Update 09_find_email_addresses.py

--- 09_find_email_addresses.py
+++ 09_find_email_addresses.py
@@ -11,10 +11,18 @@ Carol Peletier carol.peletier@yahoo.com Atlanta,
 Maggie Greene mags313@gmail.com n/a
 """
 
+# Used method:
+# findall()         returns all matches within a string and returns the matches in a list.
+# 
 reg_ex_pattern = r'[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}'
 reg_ex = re.compile(reg_ex_pattern, flags=re.IGNORECASE)
 result = reg_ex.findall(output)
 print(result)
-
+#
 # Used method:
-# findall()         returns all matches within a string and returns the matches in a list.
+# search()          returns a re-object containing information about the location - 
+#                   beginning and end - of the expression within the string.
+#
+fm = reg_ex.search(output)              # fm = first match
+result = output[fm.start():fm.end()]    # use the position of beginning and end for locating the 
+print(result)                           # search result of the first match within the string.
