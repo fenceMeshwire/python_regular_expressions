6df4623b4c16097607c897de7b5984c1a23010ac
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-07-14 13:25:23 +0200

    Create 11_negative_lookahead.py

---
+++ 11_negative_lookahead.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 11_negative_lookahead.py
+
+# Dependency
+import re
+
+undesired = re.compile('^(?!.*\d{4}).*$')
+
+samples = ['AB34', '3410']
+
+result = [samples[i] for i in range(len(samples)) if re.match(undesired, samples[i])]
+
+print(result)
